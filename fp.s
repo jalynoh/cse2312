6e4cdb8222fee4fb12afab256b580d0d383bcb20
puor <puor@live.com>
2016-12-10 15:07:37 -0600

    test fp

--- fp.s
+++ fp.s
@@ -41,7 +41,7 @@ _scanf:
     LDR R0, =format_str     @ R0 contains address of format string
     MOV R1, SP              @ move SP to R1 to store entry on stack
     BL scanf                @ call scanf
-    LDR R0, [SP]            @ load value at SP into R0
+    LDR R1, [SP]            @ load value at SP into R0
     ADD SP, SP, #4          @ restore the stack pointer
     POP {PC}                 @ return
 
