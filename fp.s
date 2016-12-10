849227d810c068a2b36ca1b2691cdcc002865989
puor <puor@live.com>
2016-12-09 21:32:19 -0600

    test fp

--- fp.s
+++ fp.s
@@ -90,7 +90,7 @@ _pow:
 		VMUL.F32 S0, S0, S1
 	_powloopcheck:
 		CMP R0, #0
-		BNE _powloop
+		BEQ _powloop
 	VCVT.F64.F32 D1, S0		@ convert single to double
 	VMOV R1, R2, D1			@ split double VFP register into two ARM registers
 	BL _printf				@ print result
