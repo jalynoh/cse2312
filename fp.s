9e40d4336663bc205428ccf76706f1b3ab892086
puor <puor@live.com>
2016-12-09 18:50:34 -0600

    test fp

--- fp.s
+++ fp.s
@@ -20,15 +20,15 @@
 main:
 	BL _vscanf				@ jump to vault scanf
 	VMOV S1, R0				@ move return value to FPU registers
-	VCVT.F32.U32 S1, S1		@ 
+	VCVT.F64.U64 S1, S1		@ 
 
 @	BL _getchar				@ operation input
 @	MOV R9, R0 				@ move operation character for later use
 @	BL _scanop				@ decifier operation input
 @	VCVT.F64.U32 D4, S0		@ convert the result to double precision
 
-	VCVT.F64.U32 D4, S1
-	VMOV R1, R2, D4			@ split double VFP register into two ARM registers
+	VCVT.F64.U32 D1, S1
+	VMOV R1, R2, D1			@ split double VFP register into two ARM registers
 	BL _printf				@ print result
 	B main					@ continuous loop
 
