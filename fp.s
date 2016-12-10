efda293e3938dea201a8c10d094823fbda77fce3
puor <puor@live.com>
2016-12-09 19:25:53 -0600

    test fp

--- fp.s
+++ fp.s
@@ -67,7 +67,7 @@ _printf:
 	POP {PC}				@ return to calling function
 
 _abs:
-	PUSH{LR}
+	PUSH {LR}
 	VABS.F32 S0, S0
 	VCVT.F64.F32 D1, S0		@ convert single to double
 	VMOV R1, R2, D1			@ split double VFP register into two ARM registers
